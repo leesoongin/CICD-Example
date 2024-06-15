1e82064d1f7918a1a330232415f600cd06ac8c05
mumu <tnddls2ek@naver.com>
2024-06-15 13:52:09 +0900

    initial commit

---
+++ CICD-Example_mumu_2/CICD-Example_mumu_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICD-Example_mumu_2
+//
+//  Created by 이숭인 on 6/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CICD-Example_mumu_2/CICD-Example_mumu_2Tests/CICD_Example_mumu_2Tests.swift
@@ -0,0 +1,28 @@
+//
+//  CICD_Example_mumu_2Tests.swift
+//  CICD-Example_mumu_2Tests
+//
+//  Created by 이숭인 on 6/15/24.
+//
+
+import XCTest
+@testable import CICD_Example_mumu_2
+
+final class CICD_Example_mumu_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_Success() {
+        XCTAssert(true)
+    }
+    
+//    func test_Fail() {
+//        
+//    }
+}
